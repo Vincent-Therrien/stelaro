5f3fe81aabd838aee6fd5be015cab965c2239992
Vincent Therrien <vincentstherrien@gmail.com>
2024-10-27 15:37:46 -0400

    Data formatting

--- src/io/format.rs
+++ src/io/format.rs
@@ -3,12 +3,36 @@
 use std::collections::HashMap;
 
 lazy_static! {
-    static ref IUB_IUPAC_nt_code: HashMap<char, u32> = {
+    static ref IUPAC_nt_code_full: HashMap<char, u32> = {
         let mut m = HashMap::new();
-        m.insert('A', 0b00000000);
-        m.insert('A', 0b00000000);
-        m.insert('A', 0b00000000);
-        m.insert('A', 0b00000000);
+        m.insert('A', 0b1000); // Adenosine
+        m.insert('C', 0b0100); // Cytosine
+        m.insert('G', 0b0010); // Guanine
+        m.insert('T', 0b0001); // Thymine
+        m.insert('U', 0b0001); // Uracil
+        m.insert('R', 0b1010); // Purine (A or G)
+        m.insert('Y', 0b0101); // Pyrimidine (C, T, or U)
+        m.insert('M', 0b1100); // Amino group (A or C)
+        m.insert('K', 0b0011); // Keto group (G, T, or U)
+        m.insert('S', 0b0110); // Strong interaction (C or G)
+        m.insert('W', 0b1001); // Weak interaction (A, T, or U)
+        m.insert('H', 0b1101); // Not G (A, T, C, or U)
+        m.insert('B', 0b0111); // Not A (A, C, T, or U)
+        m.insert('V', 0b1110); // Not T or U (A, C, or G)
+        m.insert('C', 0b1011); // Not C (A, G, T, or U)
+        m.insert('N', 0b1111); // Any (A, C, G, T, or U)
+        m
+    };
+}
+
+lazy_static! {
+    static ref IUPAC_nt_code_compressed: HashMap<char, u32> = {
+        let mut m = HashMap::new();
+        m.insert('A', 0b00); // Adenosine
+        m.insert('C', 0b01); // Cytosine
+        m.insert('G', 0b10); // Guanine
+        m.insert('T', 0b11); // Thymine
+        m.insert('U', 0b11); // Uracil
         m
     };
 }
