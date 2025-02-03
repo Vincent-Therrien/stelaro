43b8d1427d6e347d063da368788b66d6d59b6653
Vincent Therrien <vincentstherrien@gmail.com>
2025-02-03 00:17:01 -0500

    Update tests

--- tests/python/test_io.py
+++ tests/python/test_io.py
@@ -18,7 +18,7 @@ FASTQ = "../data/nucleotide_sequence.fastq"
 
 def test_read_fasta():
     os.chdir(dname)
-    result = stelaro_rs.read_fasta(FASTA)
+    result = stelaro_rs.read_fasta(FASTA)["obj"]
     assert len(result) == 2, "unexpected count."
     assert result[0][1] == "AACCGGTTAACCGGTT", "Unexpected sequence"
     assert result[1][1] == "AACCGGTT", "Unexpected sequence"
@@ -26,10 +26,11 @@ def test_read_fasta():
 
 def test_read_fastq():
     os.chdir(dname)
-    result = stelaro_rs.read_fastq(FASTQ)
+    result = stelaro_rs.read_fastq(FASTQ)["obj"]
     print(result)
     assert len(result) == 3
     assert result[0][1] == "AACCGGTTAACCGGTT", "Unexpected sequence"
-    assert result[0][2] == [
+    quality = [int(b) for b in result[0][2]]
+    assert quality == [
         0, 0, 0, 93, 93, 93, 93, 93, 0, 0, 0, 93, 93, 93, 93, 93
     ]
