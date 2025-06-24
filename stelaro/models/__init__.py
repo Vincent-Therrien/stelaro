3d3dec754259fdb5ffdc0f9c343e21914804a7d7
Vincent Therrien <vincentstherrien@gmail.com>
2025-06-24 17:25:21 -0400

    Evaluate at multiple ranks

--- stelaro/models/__init__.py
+++ stelaro/models/__init__.py
@@ -8,7 +8,8 @@
 """
 
 from sklearn.metrics import f1_score
-from torch import tensor, no_grad,argmax, half, float32
+from torch import tensor, no_grad,argmax, half, float32, Tensor
+from torch.nn import functional
 from torch.nn import Module
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
@@ -16,7 +17,13 @@ from tqdm import tqdm
 
 
 class SyntheticReadDataset(Dataset):
+    """Dataset containing one-hot encoded synthetic reads."""
     def __init__(self, directory: str, mapping: str = None):
+        """Args:
+            directory: Path of the directory that contains the x and y files.
+            mapping: Either `None` (all in main memory) or `"r"`
+                (memory-mapped, useful for very large datasets).
+        """
         self.x = np.load(directory + "x.npy", mmap_mode=mapping)
         self.y = np.load(directory + "y.npy", mmap_mode=mapping)
 
@@ -30,11 +37,58 @@ class SyntheticReadDataset(Dataset):
         return tensor(x), tensor(y)
 
 
+def obtain_rank_based_mappings(mapping: dict) -> list[dict]:
+    """Convert the flat-level indices to higher taxonomic rank indices."""
+    keys = []
+    mappings = []
+    for i in range(len(mapping['0']) - 1, -1, -1):
+        keys.append(set())
+        mappings.append({})
+        for j, labels in mapping.items():
+            label = labels[i]
+            if label not in keys[-1]:
+                keys[-1].add(label)
+            mappings[-1][int(j)] = len(keys[-1]) - 1
+        if i == len(mapping['0']) - 1:
+            assert len(keys[-1]) == len(mapping), "Duplicate names."
+    return mappings
+
+
+def rank_based_f1_score(
+        mappings: dict, target: list[int], predictions: list[int]
+        ) -> list[np.ndarray]:
+    """Evaluate F1 score at multiple taxonomic ranks."""
+    f = []
+    for mapping in mappings:
+        labels = list(set(mapping.values()))
+        normalized_target = [mapping[int(v)] for v in list(target)]
+        normalized_pred = [mapping[int(v)] for v in list(predictions)]
+        f.append(
+            f1_score(
+                normalized_target,
+                normalized_pred,
+                average=None,
+                labels=labels,
+                zero_division=0.0
+            )
+        )
+    return f
+
+
+def penalized_cross_entropy(logits, targets, penalty_matrix):
+    log_probs = functional.log_softmax(logits, dim=1)
+    probs = log_probs.exp()
+    batch_size = logits.size(0)
+    penalties = penalty_matrix[targets]
+    weighted_loss = (probs * penalties).sum(dim=1)
+    return weighted_loss.mean()
+
+
 def evaluate(model, loader, device, mapping):
     """Evaluate the F1 score at different taxonomic levels."""
     model.eval()
-    labels = [int(m) for m in list(mapping.keys())]
-    f = np.zeros((len(labels)))
+    mappings = obtain_rank_based_mappings(mapping)
+    ranks = []
     n_batches = 0
     with no_grad():
         for x_batch, y_batch in loader:
@@ -42,19 +96,16 @@ def evaluate(model, loader, device, mapping):
             x_batch = x_batch.permute(0, 2, 1)  # Swap channels and sequence.
             y_batch = y_batch.to("cpu")
             predictions = argmax(model(x_batch), dim=1).to("cpu")
-            # TODO: Account for taxonomic level.
-            f_scores = f1_score(
-                y_batch,
-                predictions,
-                average=None,
-                labels=labels,
-                zero_division=0.0
-            )
+            ranks.append(rank_based_f1_score(mappings, y_batch, predictions))
             n_batches += 1
-            for i in range(len(f_scores)):
-                f[i] += f_scores[i]
-    f /= n_batches
-    return f
+    collapsed_ranks = [np.zeros(len(r)) for r in ranks[0]]
+    for rank in ranks:
+        for i, result in enumerate(rank):
+            collapsed_ranks[i] += result
+    for i in range(len(collapsed_ranks)):
+        collapsed_ranks[i] = np.mean(collapsed_ranks[i])
+        collapsed_ranks[i] /= n_batches
+    return collapsed_ranks
 
 
 def train(
@@ -99,14 +150,21 @@ def train(
             loss.backward()
             optimizer.step()
             losses[-1] += loss.item()
-        f = np.mean(evaluate(model, validate_loader, device, mapping))  # TODO: Taxa
-        if f > best_f1:
-            f = best_f1
-        print(f"{epoch+1}/{max_n_epochs} Loss: {losses[-1]:.2f}. F1: {f:.3f}")
-        average_f_scores.append(f)
-        if patience and f < best_f1:
+        f1 = evaluate(model, validate_loader, device, mapping)
+        f1 = [float(f) for f in f1]
+        average_f_scores.append(f1)
+        if f1[0] > best_f1:
+            best_f1 = f1[0]
+        if f1[0] < best_f1:
             patience -= 1
-            if patience < 0:
-                print("Stopping early.")
-                break
+        print(
+            f"{epoch+1}/{max_n_epochs}",
+            f"Loss: {losses[-1]:.2f}.",
+            "F1: ", [f"{f:.5}" for f in f1], " .",
+            f"Patience: {patience}"
+        )
+        if patience < 0:
+            print("Stopping early.")
+            break
+    average_f_scores = list(np.array(average_f_scores).T)
     return losses, average_f_scores
--- stelaro/models/feedforward.py
+++ stelaro/models/feedforward.py
@@ -7,22 +7,72 @@
     - License: MIT
 """
 
-from torch import nn
+from torch.nn import Module, Conv1d, ReLU, Sequential, Flatten, Linear, Softmax
 
-class CNN_1(nn.Module):
+
+class MLP_1(Module):
+    def __init__(self, N, M):
+        super(MLP_1, self).__init__()
+        self.conv = Sequential(
+            Conv1d(4, 1, kernel_size=3, padding=1),
+            ReLU(),
+        )
+        self.fc = Sequential(
+            Flatten(),
+            Linear(N, int(N / 2)),
+            ReLU(),
+            Linear(int(N / 2), int(N / 4)),
+            ReLU(),
+            Linear(int(N / 4), M)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.fc(x)
+        return x.to(float)
+
+
+
+class CNN_1(Module):
     def __init__(self, N, M):
         super(CNN_1, self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv1d(4, 32, kernel_size=7, padding=3),
-            nn.ReLU(),
-            nn.Conv1d(32, 64, kernel_size=5, padding=2),
-            nn.ReLU(),
+        self.conv = Sequential(
+            Conv1d(4, 32, kernel_size=7, padding=3),
+            ReLU(),
+            Conv1d(32, 64, kernel_size=5, padding=2),
+            ReLU(),
         )
-        self.fc = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(N * 64, 128),
-            nn.ReLU(),
-            nn.Linear(128, M)
+        self.fc = Sequential(
+            Flatten(),
+            Linear(N * 64, 128),
+            ReLU(),
+            Linear(128, M)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.fc(x)
+        return x.to(float)
+
+
+class CNN_2(Module):
+    def __init__(self, N, M):
+        super(CNN_2, self).__init__()
+        self.conv = Sequential(
+            Conv1d(4, 32, kernel_size=7, padding=3),
+            ReLU(),
+            Conv1d(32, 64, kernel_size=5, padding=2),
+            ReLU(),
+            Conv1d(64, 128, kernel_size=3, padding=1),
+            ReLU(),
+        )
+        self.fc = Sequential(
+            Flatten(),
+            Linear(N * 128, int(N / 2)),
+            ReLU(),
+            Linear(int(N / 2), 128),
+            ReLU(),
+            Linear(128, M)
         )
 
     def forward(self, x):
